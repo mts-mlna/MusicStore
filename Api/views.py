0290c4e573082b67b26166f25380d5dc4068ae41
mts-mlna <matiasezequielmolina2006@gmail.com>
2025-07-08 14:41:41 -0300

    asassas

--- Api/views.py
+++ Api/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.db.models import Q
-<<<<<<< HEAD
 from .models import *
-=======
-from models import *
->>>>>>> 3cf89320438abaed263153404ec7a1ddc2e476f3
 
 # Create your views here.
 def Home(request):
