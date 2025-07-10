9070b40f8739b29ca8fcb2b490d426a8fe663fa7
Bruno Ezequiel Sánchez <3zequielbsanchez@gmail.com>
2025-07-10 15:09:25 -0300

    FFFFFF

--- Api/models.py
+++ Api/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here
 class Producto(models.Model):
+    Codigo = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    imagen = models.ImageField()
+    imagen = models.ImageField(upload_to="Musimg", null=True)
 
-    def __str__(self):
-        return self.nombre
+    def __int__(self):
+        return self.Codigo
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
--- Api/views.py
+++ Api/views.py
@@ -1,36 +1,90 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required 
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login,authenticate
+from .forms import CustomUserCreationForm 
+from .forms import *
+import time
 from django.db.models import Q
 from .models import *
+
+
+
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 def Home(request):
     return render (request, "index.html")
 
-def RegistrarProducto(request):
+@login_required
+
+
+def Salir(request):
+    logout(request)
+    return redirect('Home')
+
+
+def Buscar(request):
+    data = {
+        'buscarform': ProductoForm()
+    }
+    if request.GET.get('buscar'):
+        buscar = request.GET.get('buscar')
+        data['productos'] = Producto.objects.filter(
+            Q(nombre__icontains=buscar) | Q(descripcion__icontains=buscar)
+        )
+    else:
+        data['productos'] = Producto.objects.all()
+    return render(request, 'Pages/Buscar.html', data)
+
+
+def Register(request):
+    data = {
+        'signup': CustomUserCreationForm()
+    }
+    query = CustomUserCreationForm(data=request.POST)
+    if query.is_valid():
+        query.save()
+        user = authenticate(username=query.cleaned_data['username'],
+                            password=query.cleaned_data['password1'])
+        login(request, user)
+        data["mensaje"] = "mati mete el script aca"
+        time.sleep(1)
+        return redirect('Home')
+    data['form'] = query
+    return render(request, 'registration/register.html', data)
+
+def producto(request):
+    query= Producto.objects.all()
+    data={
+        'productos':query
+    }
+    return render(request,"pages/Productos.html", data)
+
+def iniciarsesion(request):
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        descripcion = request.POST.get('descripcion')
-        precio = request.POST.get('precio')
-        stock = request.POST.get('stock')
-        imagen = request.FILES.get('imagen')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('Home')
+        else:
+            return render(request, 'registration/login.html', {'error': 'Credenciales inválidas'})
+    return render(request, 'registration/login.html')
 
-        producto = Producto(nombre=nombre, descripcion=descripcion, precio=precio, stock=stock, imagen=imagen)
-        producto.save()
-
-    return render()
-
-def RegistrarCliente(request):
+def Registro(request):
+    data={
+        'regproducto':ProductoForm()
+    }
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        apellido = request.POST.get('apellido')
-        email = request.POST.get('email')
-        telefono = request.POST.get('telefono')
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            data['mensaje'] = "Producto agregado correctamente"
+        else:
+            data['regproducto'] = ProductoForm()
+    return render(request, 'Pages/Registro.html', data)
 
-        cliente = Cliente(nombre=nombre, apellido=apellido, email=email, telefono=telefono)
-        cliente.save()
 
-    return render()
 
 
